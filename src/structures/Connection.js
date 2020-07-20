127b94e759c3177d8105274243c211dc474fea6e
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-20 17:49:35 -0300

    chore: add "subscribe" method

--- src/structures/Connection.js
+++ src/structures/Connection.js
@@ -1,8 +1,13 @@
+const { EventEmitter } = require('events');
+
 const WebSocket = require('../connection/WebSocket');
 const Subscriptions = require('../connection/Subscriptions');
+const { STATUS, EVENTS } = require('../utils/Constants');
 
-class Connection {
+class Connection extends EventEmitter {
   constructor(url, websocketOptions = {}) {
+    super();
+
     this.ws = new WebSocket(this, Object.assign(websocketOptions, { url }));
     this.subscriptions = new Subscriptions();
   }
@@ -10,6 +15,27 @@ class Connection {
   connect() {
     if (!this.ws.connection) this.ws.connect();
     return this;
+  }
+
+  /**
+   * @param {string} topic
+   */
+  subscribe(topic) {
+    if (this.subscriptions.has(topic)) return this.subscriptions.get(topic);
+
+    const socket = this.subscriptions.add(topic, this, topic);
+
+    socket.on(EVENTS.JOIN, () => {
+      this.emit(EVENTS.SUBSCRIPTION_JOIN, topic);
+    });
+
+    socket.on(EVENTS.CLOSE, () => {
+      if (this.subscriptions.delete(topic)) this.subscribe(topic);
+      this.emit(EVENTS.SUBSCRIPTION_CLOSE, topic);
+    });
+
+    if (this.ws.status === STATUS.READY) this.ws.joinSubscription(topic);
+    return socket;
   }
 }
 
