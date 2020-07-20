4a59bc79ccefd3d1db8409495129db3a612461c4
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-07-20 17:50:51 -0300

    constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,9 +1,9 @@
 module.exports.STATUS = {
-  OPEN: 0,
-  CONNECTING: 1,
-  RECONNECTING: 2,
-  IDLE: 3,
-  NEARLY: 4,
+  IDLE: 0,
+  READY: 1,
+  WAITING: 2,
+  CONNECTING: 3,
+  RECONNECTING: 4,
   DISCONNECTED: 5,
 };
 
@@ -18,4 +18,18 @@ module.exports.CODES = {
   EVENT: 7,
   PING: 8,
   PONG: 9,
+};
+
+module.exports.EVENTS = {
+  JOIN: 'join',
+  READY: 'ready',
+  CLOSE: 'close',
+  ERROR: 'error',
+  LEAVE: 'leave',
+
+  JOIN_ERROR: 'joinError',
+  LEAVE_ERROR: 'leaveError',
+
+  SUBSCRIPTION_JOIN: 'subscriptionJoin',
+  SUBSCRIPTION_CLOSE: 'subscriptionClose',
 };
